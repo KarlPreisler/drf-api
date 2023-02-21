0609a85d4347d388bf71aa1600f8ee4a2be650b4
KarlPreisler <Karlpreisler@gmail.com>
2023-02-21 15:29:41 +0000

    fix bug by adding imports to follower views

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,9 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+
 class FollowerList(generics.ListCreateAPIView):
     """
     List all followers, i.e. all instances of a user
